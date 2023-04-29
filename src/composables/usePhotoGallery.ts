0d9470ff34c442b4666b0ac61c99b92218d7b84d
Nikkei96 <nikitarudentsov@yandex.ru>
2023-04-30 01:27:21 +0300

    add save and load photos from indexedDB

--- src/composables/usePhotoGallery.ts
+++ src/composables/usePhotoGallery.ts
@@ -15,6 +15,64 @@ export interface UserPhoto {
 
 export const usePhotoGallery = () => {
   const photos = ref<UserPhoto[]>([]);
+  const PHOTO_STORAGE = 'photos';
+
+  const convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
+
+  const cachePhotos = () => {
+    Preferences.set({
+      key: PHOTO_STORAGE,
+      value: JSON.stringify(photos.value),
+    });
+  };
+
+  const loadSaved = async () => {
+    const photoList = await Preferences.get({ key: PHOTO_STORAGE });
+    const photosInPreferences = photoList.value
+      ? JSON.parse(photoList.value)
+      : [];
+
+    for (const photo of photosInPreferences) {
+      const file = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: Directory.Data,
+      });
+      photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+    }
+
+    photos.value = photosInPreferences;
+  };
+
+  const savePicture = async (
+    photo: Photo,
+    fileName: string,
+  ): Promise<UserPhoto> => {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    const base64Data = (await convertBlobToBase64(blob)) as string;
+
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
+  };
 
   const takePhoto = async () => {
     const photo = await Camera.getPhoto({
@@ -24,13 +82,13 @@ export const usePhotoGallery = () => {
     });
 
     const fileName = new Date().getTime() + '.jpeg';
-    const savedFileImage = {
-      filepath: fileName,
-      webviewPath: photo.webPath,
-    };
+    const savedFileImage = await savePicture(photo, fileName);
 
     photos.value = [savedFileImage, ...photos.value];
   };
+
+  watch(photos, cachePhotos);
+  onMounted(loadSaved);
 
   return {
     takePhoto,
