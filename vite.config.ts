cd97f70ca541a4190871dca08a31c5a0fec24c7c
Nikkei96 <nikitarudentsov@yandex.ru>
2023-04-29 23:44:47 +0300

    run prettier --write

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import legacy from '@vitejs/plugin-legacy'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import legacy from '@vitejs/plugin-legacy';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
+  plugins: [vue(), legacy()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -16,6 +14,6 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: 'jsdom'
-  }
-})
+    environment: 'jsdom',
+  },
+});
