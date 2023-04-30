c78dc539e863fa691d3b08e57a78498b1bd85b31
Nikkei96 <nikitarudentsov@yandex.ru>
2023-04-30 18:40:24 +0300

    make capacitor apps

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.starter.new.app.get',
   appName: 'photo-gallery',
   webDir: 'dist',
   bundledWebRuntime: false,
